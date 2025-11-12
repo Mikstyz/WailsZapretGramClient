8caade962b3bea1546abab041e64ff97c91b70e7
Арсюша <dosyroe@gmail.com>
2025-11-12 15:51:02 +0300

    papa

--- ZapretGram/main.go
+++ ZapretGram/main.go
@@ -11,7 +11,7 @@ import (
 	_ "modernc.org/sqlite"
 )
 
-//go:embed all:frontend/pages all:frontend/css all:frontend/dist
+//go:embed all:frontend/pages all:frontend/dist
 var assets embed.FS
 
 func main() {
